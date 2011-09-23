9b40b41c58b540a31bbdf2d3fdc79de776a6a8bf
Chris Cooper <chriscooper1991@gmail.com>
2011-09-23 16:19:35 -0400

    Scraping all courses, only getting titles right now.

---
+++ selenium/SolusModels.py
@@ -0,0 +1,8 @@
+class SolusCourse:
+    
+    num_courses = 0
+    
+    def __init__(self):
+        self.title = ""
+        self.subject = ""
+        self.num = ""
--- selenium/solus_scraper.py
+++ selenium/solus_scraper.py
@@ -1,5 +1,6 @@
 from selenium import selenium
 import unittest, time, re
+from SolusModels import SolusCourse
 
 def wait_then_click(sel, identifier):
     while not sel.is_element_present(identifier):
@@ -8,10 +9,13 @@ def wait_then_click(sel, identifier):
     sel.click(identifier)
 
 class selenium_export(unittest.TestCase):
+    
     def setUp(self):
         self.verificationErrors = []
         self.selenium = selenium("localhost", 4444, "*chrome", "https://sso.queensu.ca/amserver/UI/Login")
         self.selenium.start()
+        
+        self.courses = []
     
     def test_selenium_export(self):
         sel = self.selenium
@@ -24,8 +28,7 @@ class selenium_export(unittest.TestCase):
             for line in config_file:
                 login_info[line_num] = line.strip()
                 line_num += 1
-                
-                
+          
         #Enter Credentials
         sel.type("id=IDToken1", login_info[0])
         sel.type("id=IDToken2", login_info[1])
@@ -52,8 +55,9 @@ class selenium_export(unittest.TestCase):
         
         print "Navigation to SOLUS complete. Beginning scraping..."
         
-        alphanums = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
         
+        #Go through all the course catalogue pages
+        alphanums = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
         for alphanum in alphanums:
             self.scrape_courses_for_alphanum(alphanum)
         
@@ -69,14 +73,18 @@ class selenium_export(unittest.TestCase):
         link_name = link_name_base % (link_number,)
         
         while sel.is_element_present(link_name):
+            #Open the dropdown
             sel.click(link_name)
             sel.wait_for_page_to_load("30000")
             
+            #Traverses all course links in the dropdown
             self.scrape_single_dropdown()
             
+            #Close the dropdown
             sel.click(link_name)
             sel.wait_for_page_to_load("30000")
             
+            #Go to next link
             link_number += 1
             link_name = link_name_base % (link_number,)
         
@@ -90,18 +98,35 @@ class selenium_export(unittest.TestCase):
         link_name = link_name_base % (link_number,)
         
         while sel.is_element_present(link_name):
+            #Go into the course
             sel.click(link_name)
             sel.wait_for_page_to_load("30000")
             
+            #Scrape info from course
             self.scrape_single_course()
             
+            #Back out from course page
+            sel.click("id=DERIVED_SAA_CRS_RETURN_PB")
+            sel.wait_for_page_to_load("30000")
+            
+            #Go to next course
             link_number += 1
             link_name = link_name_base % (link_number,)
     
     def scrape_single_course(self):
         sel = self.selenium
         
-        time.sleep(2)
+        course = SolusCourse()
+        
+        raw_title = sel.get_text("css=span.PALEVEL0SECONDARY").strip()
+        
+        m = re.search('^([\S]+)\s+([\S]+)\s+-\s+(.*)$', raw_title)
+        
+        course.subject = m.group(1)
+        course.num = m.group(2)
+        course.title = m.group(3)
+
+        print "%s %s - %s" % (course.subject, course.num, course.title)
     
     def uncopied(self):
         sel.click("id=CRSE_TITLE$0")
