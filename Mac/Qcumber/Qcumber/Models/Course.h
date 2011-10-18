6d8a77113a0b0ae4888b01072675e1ef95222d95
Chris Cooper <chriscooper1991@gmail.com>
2011-10-18 05:09:29 -0400

    Model schema updated to reflect JSON references

---
+++ Mac/Qcumber/Qcumber/Models/Course.h
@@ -0,0 +1,13 @@
+//
+//  Course.h
+//  Qcumber
+//
+//  Created by Chris Cooper on 11-10-14.
+//  Copyright 2011 Chris Cooper. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Course : NSObject
+
+@end
---
+++ Mac/Qcumber/Qcumber/Models/Course.m
@@ -0,0 +1,23 @@
+//
+//  Course.m
+//  Qcumber
+//
+//  Created by Chris Cooper on 11-10-14.
+//  Copyright 2011 Chris Cooper. All rights reserved.
+//
+
+#import "Course.h"
+
+@implementation Course
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
