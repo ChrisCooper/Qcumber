7e0ac301b46eac32f14dec8ac22c29754ffec10f
Chris Cooper <chriscooper1991@gmail.com>
2011-10-24 16:30:17 -0400

    Started laying out course box

---
+++ Mac/Qcumber/Qcumber/CourseBoxViewController.m
@@ -0,0 +1,23 @@
+//
+//  CourseBoxViewController.m
+//  Qcumber
+//
+//  Created by Chris Cooper on 11-10-24.
+//  Copyright 2011 Chris Cooper. All rights reserved.
+//
+
+#import "CourseBoxViewController.h"
+
+@implementation CourseBoxViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
