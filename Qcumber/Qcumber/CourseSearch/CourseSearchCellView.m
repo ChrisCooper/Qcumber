7577144ab1197339a0644da00a08f76f4e7008ff
Chris Cooper <chriscooper1991@gmail.com>
2012-01-07 21:06:37 -0500

    Added cell for courses in serach results

---
+++ Qcumber/Qcumber/CourseSearch/CourseSearchCellView.m
@@ -0,0 +1,19 @@
+//
+//  CourseSearchCellView.m
+//  Qcumber
+//
+//  Created by Chris Cooper on 12-01-03.
+//  Copyright (c) 2012 Chris Cooper. All rights reserved.
+//
+
+#import "CourseSearchCellView.h"
+
+@implementation CourseSearchCellView
+@synthesize codeLabel;
+@synthesize titleLabel;
+@synthesize favoriteButton;
+@synthesize descriptionLabel;
+@synthesize sectionTypeLabels;
+@synthesize creditsLabel;
+
+@end
