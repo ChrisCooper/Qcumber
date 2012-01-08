d72c22e58dafa2a5e07c7b1ad2e73da79ba02fd1
Chris Cooper <chriscooper1991@gmail.com>
2012-01-08 01:19:48 -0500

    mis again

---
+++ Qcumber/Qcumber/CourseSearch/CourseSearchAndTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  CourseSearchAndTableViewController.h
+//  Qcumber
+//
+//  Created by Chris Cooper on 12-01-08.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface CourseSearchAndTableViewController : NSViewController
+
+@end
---
+++ Qcumber/Qcumber/CourseSearch/CourseSearchAndTableViewController.m
@@ -0,0 +1,23 @@
+//
+//  CourseSearchAndTableViewController.m
+//  Qcumber
+//
+//  Created by Chris Cooper on 12-01-08.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "CourseSearchAndTableViewController.h"
+
+@implementation CourseSearchAndTableViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
